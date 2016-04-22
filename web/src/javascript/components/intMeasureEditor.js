cb37ba88252faa38b774ef859ca9f313015e372b
Alan Holt <aholt@venturetech.net>
2016-04-22 10:02:44 -0500

    fixed parseInt to use radix  10

--- web/src/javascript/components/intMeasureEditor.js
+++ web/src/javascript/components/intMeasureEditor.js
@@ -2,9 +2,9 @@
  * Created by vtdev on 4/19/16.
  */
 function onIntMeasureInput(element, targetName, targetLabelId) {
-	var val = parseInt(element.value);
+	var val = parseInt(element.value, 10);
 	var target = document.getElementsByName(targetName)[0];
-	if(target.type.toLowerCase() == 'range' && parseInt(target.max) < val) {
+	if(target.type.toLowerCase() == 'range' && parseInt(target.max, 10) < val) {
 		target.max = val;
 	}
 	target.value = val;
