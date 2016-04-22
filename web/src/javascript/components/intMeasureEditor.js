c4b814e357778b1aa7981a3c65392cd3625dd43b
Alan Holt <aholt@venturetech.net>
2016-04-22 09:55:41 -0500

    fixed intMeasureEditor javascript

--- web/src/javascript/components/intMeasureEditor.js
+++ web/src/javascript/components/intMeasureEditor.js
@@ -2,12 +2,10 @@
  * Created by vtdev on 4/19/16.
  */
 function onIntMeasureInput(element, targetName, targetLabelId) {
-	var val = element.value;
+	var val = parseInt(element.value);
 	var target = document.getElementsByName(targetName)[0];
-	if(target.type.toLowerCase() == 'range' && target.max < val) {
-		var targetLabel = document.getElementById(targetLabelId);
+	if(target.type.toLowerCase() == 'range' && parseInt(target.max) < val) {
 		target.max = val;
-		targetLabel.innerHTML = val;
 	}
 	target.value = val;
 }
