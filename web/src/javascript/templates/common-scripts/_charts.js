032037667785d20ddc7605436cf5f00986f752ac
djacobi-venturetech <djacobi@venturetech.net>
2016-04-27 12:52:09 -0500

    Rebase from upstream/master

--- web/src/javascript/templates/common-scripts/_charts.js
+++ web/src/javascript/templates/common-scripts/_charts.js
@@ -1,8 +1,6 @@
-//noinspection JSUnresolvedVariable
 /**
  * Created by vtdev on 4/7/16.
  */
-
 function setupCharts(ctx) {
 	var $ctx = !!ctx ? $(ctx.parentNode || document) : $(ctx || document);
 	$ctx.find('[data-chart-type]').each(function(idx, target) {
@@ -14,27 +12,45 @@ function setupCharts(ctx) {
 }
 
 function setupChart(target) {
+	function addCanvas(target) {
+		var canvases = target.getElementsByTagName("canvas");
+		for (var i = 0; i < canvases.length; i++) {
+			target.removeChild(canvases[i]);
+		}
+		var canvas = document.createElement("canvas");
+		target.appendChild(canvas);
+		return canvas;
+	}
+	function clearSvgs(target) {
+		var svgs = target.getElementsByTagName("svg");
+		for (var i = 0; i < svgs.length; i++) {
+			target.removeChild(svgs[i]);
+		}
+	}
 	var CHART_TYPES = function chartTypesInit() {
 		var gauge = {
 			name: "gauge",
 			isLegendSupported: false,
-			createChart: function setupGauge(canvas, data, options) {
-				canvas.setAttribute("height", "200");
-				canvas.setAttribute("width", "200");
+			createChart: function setupGauge(target, data, options) {
+				clearSvgs(target);
+				options.id = target.getAttribute("id");
+				//noinspection JSUnresolvedVariable
+				options.value = data.currVal;
 				//noinspection JSUnresolvedFunction
-				var chart = new Donut(canvas).setOptions(options);
-				//noinspection JSUnresolvedVariable
-				chart.maxValue = data.maxVal;
-				chart.animationSpeed = 32;
-				//noinspection JSUnresolvedVariable
-				chart.set(data.currVal);
+				var chart = new JustGage(options);
+
+				target.style.height = "207px";
+				target.style.width = "325px";
+
 				return chart;
 			}
 		};
 		var pie = {
 			name: "pie",
 			isLegendSupported: true,
-			createChart: function setupPie(canvas, data, options) {
+			canvasSupported: true,
+			createChart: function setupPie(target, data, options) {
+				var canvas = addCanvas(target);
 				var canvasContext = canvas.getContext('2d');
 				canvas.setAttribute("height", "150");
 				canvas.setAttribute("width", "150");
@@ -45,10 +61,13 @@ function setupChart(target) {
 		var line = {
 			name: "line",
 			isLegendSupported: true,
-			createChart: function setupLine(canvas, data, options) {
+			createChart: function setupLine(target, data, options) {
+				var canvas = addCanvas(target);
 				var canvasContext = canvas.getContext('2d');
-				//canvas.setAttribute("height", "200");
-				//canvas.setAttribute("width", "400");
+				canvas.setAttribute("height", "200");
+				canvas.setAttribute("width", "300");
+				target.style.height = "207px";
+				target.style.width = "325px";
 				//noinspection JSUnresolvedFunction
 				return new Chart(canvasContext).Line(data, options);
 			}
@@ -56,7 +75,8 @@ function setupChart(target) {
 		var bar = {
 			name: "bar",
 			isLegendSupported: true,
-			createChart: function setupBar(canvas, data, options) {
+			createChart: function setupBar(target, data, options) {
+				var canvas = addCanvas(target);
 				var canvasContext = canvas.getContext('2d');
 				canvas.setAttribute("height", "200");
 				canvas.setAttribute("width", "300");
@@ -68,7 +88,8 @@ function setupChart(target) {
 		var radar = {
 			name: "radar",
 			isLegendSupported: true,
-			createChart: function setupRadar(canvas, data, options) {
+			createChart: function setupRadar(target, data, options) {
+				var canvas = addCanvas(target);
 				var canvasContext = canvas.getContext('2d');
 				canvas.setAttribute("height", "325");
 				canvas.setAttribute("width", "400");
@@ -79,7 +100,8 @@ function setupChart(target) {
 		var polarArea = {
 			name: "polar-area",
 			isLegendSupported: true,
-			createChart: function setupPolarArea(canvas, data, options) {
+			createChart: function setupPolarArea(target, data, options) {
+				var canvas = addCanvas(target);
 				var canvasContext = canvas.getContext('2d');
 				canvas.setAttribute("height", "325");
 				canvas.setAttribute("width", "400");
@@ -108,14 +130,9 @@ function setupChart(target) {
 	var data = JSON.parse(target.getAttribute("data-data"));
 	var chartType = CHART_TYPES.getType(target.getAttribute("data-chart-type"));
 	var includeLegend = target.getAttribute("data-include-legend").toLowerCase() == "true";
-	var canvases = target.getElementsByTagName("canvas");
-	for(var i = 0; i < canvases.length; i++) {
-		target.removeChild(canvases[i]);
-	}
-	var canvas = document.createElement("canvas");
-	target.appendChild(canvas);
+
 	if(chartType != null) {
-		target.chart = chartType.createChart(canvas, data, options);
+		target.chart = chartType.createChart(target, data, options);
 		if (target.chart && includeLegend && chartType.isLegendSupported) {
 			var legends = target.getElementsByClassName("chart-legend");
 			var legend = null;
