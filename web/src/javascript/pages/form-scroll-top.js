1350500a45f484fbbc2e7e038f398d6db158228d
djacobi-venturetech <djacobi@venturetech.net>
2016-05-18 15:06:05 -0500

    Created file for form pages only. Scrolls user to top of page when clicking Back or Next button

---
+++ web/src/javascript/pages/form-scroll-top.js
@@ -0,0 +1,15 @@
+/**
+	 * Created by vtdev on 5/18/16.
+ */
+jQuery(function () {
+
+	$('form.miwt-form').each(function (idx, form) {
+		//noinspection JSUnusedGlobalSymbols
+		form.submit_options = {
+			postUpdate: function () {
+				window.scrollTo(0,0);
+			}
+		};
+	});
+
+});
